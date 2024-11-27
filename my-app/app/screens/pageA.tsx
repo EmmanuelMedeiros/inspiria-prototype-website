031dc150997f80490a0a1f0142f2c0d279c0ef64
Emmanuel <emmanuel.t2003@hotmail.com>
2024-11-27 16:12:30 -0300

    FIX - Following ESLINT rules to deploy project on Vercel 03

--- my-app/app/screens/pageA.tsx
+++ my-app/app/screens/pageA.tsx
@@ -1,12 +1,7 @@
 'use client'
 
-import { useState } from 'react';
 import StaticHeader from '../components/staticHeader'
 import style from '../styles/pageA.module.css'
-
-import localFont from "next/font/local";
-import { NextFontWithVariable } from 'next/dist/compiled/@next/font';
-
 
 export default function PageA() {
 
