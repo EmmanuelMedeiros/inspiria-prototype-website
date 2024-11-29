18f33f826e504367a3b01c43577cc0de57398d5e
Emmanuel <emmanuel.t2003@hotmail.com>
2024-11-29 11:11:37 -0300

    NEW - Implement responsivity for mobile and adjusting ESLINT rules 03

--- my-app/app/context/pageContext.tsx
+++ my-app/app/context/pageContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, Dispatch, ReactNode, SetStateAction, useState } from "react";
+import { createContext, Dispatch, SetStateAction, useState } from "react";
 import PageSection from "../enum/PageSection";
 
 interface PageContextInterface {
